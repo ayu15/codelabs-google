75660c5d2dbfbe9a1072097ae47836b99c3e5a36
Ayush <ayush15may@gmail.com>
2019-03-10 22:01:36 +0530

    Base setup

---
+++ README.md
@@ -0,0 +1,4 @@
+Collection of solved assignments from codelabs google
+https://codelabs.developers.google.com/?cat=Web
+
+hosted at https://codelabs-google.netlify.com
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Codelabs google</h1>
+          <ul>
+            <li><a href="./pages/your-first-pwapp/index.html">Your first PWA</a></li>
+          </ul>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
